d4e8fb13117e366d8cb919b42f73905ce1690639
Shuhei Yamamoto <yamamoto@360ch.tv>
2024-02-14 15:55:06 +0900

    @availableをコメントアウト

--- Framework/Sources/FloatingPanelSurfaceView.swift
+++ Framework/Sources/FloatingPanelSurfaceView.swift
@@ -96,7 +96,7 @@ public class FloatingPanelSurfaceView: UIView {
     /// content view.
     public let containerView: UIView = UIView()
 
-    @available(*, unavailable, renamed: "containerView")
+//    @available(*, unavailable, renamed: "containerView")
     public var backgroundView: UIView!
 
     private lazy var containerViewTopInsetConstraint: NSLayoutConstraint = containerView.topAnchor.constraint(equalTo: topAnchor, constant: containerTopInset)
